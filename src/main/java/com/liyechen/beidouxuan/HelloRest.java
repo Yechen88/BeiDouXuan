60abd5fb0589ffdfee4b8525c5c4c56a07450d05
liyechen <liyechen@192.168.0.106>
2017-05-25 12:07:09 +0800

    github test

--- src/main/java/com/liyechen/beidouxuan/HelloRest.java
+++ src/main/java/com/liyechen/beidouxuan/HelloRest.java
@@ -9,6 +9,27 @@ package com.liyechen.beidouxuan;
  *
  * @author liyechen
  */
+
+import javax.ws.rs.GET;  
+import javax.ws.rs.Path;  
+import javax.ws.rs.Produces;  
+import javax.ws.rs.PathParam;  
+import javax.ws.rs.core.MediaType;  
+
+
+@Path("/hello")  
 public class HelloRest {
-    
+    @GET  
+    @Produces(MediaType.TEXT_PLAIN)  
+    public String sayHello() {  
+        return "Hello World!" ;  
+    }  
+   
+      
+    @GET  
+    @Path("/{param}")    
+    @Produces("text/plain;charset=UTF-8")  
+    public String sayHelloToUTF8(@PathParam("param") String username) {  
+        return "Hello " + username;  
+    }  
 }
